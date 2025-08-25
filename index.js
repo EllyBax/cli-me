2745e345388a36fcdcfb558bd0d08720e17abaf0
Elly B <baxllycc@gmail.com>
2025-08-25 09:25:07 +0300

    The program can simply greet the user

---
+++ index.js
@@ -0,0 +1,22 @@
+import { input } from "@inquirer/prompts";
+import chalk from "chalk";
+import { Command } from "commander";
+async function main() {
+  const program = new Command("cli-me");
+  program.version("1.0.0");
+
+  program
+    .command("hello")
+    .description("say hello")
+    .action(async () => {
+      const name = await input({
+        message: "What is your name?",
+      });
+      console.log(chalk.cyan(`hello ${name}`));
+      process.exit(0);
+    });
+
+  program.parse(process.argv);
+}
+
+await main();
